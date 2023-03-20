a08d3ac2e1c178e7a47e9a1b50963b214500f72c
way0utwest <sjones@sqlservercentral.com>
2023-03-20 14:42:28 -0600

    added db drop script

---
+++ SQL/99-DropDB.sql
@@ -0,0 +1,17 @@
+/*
+
+Zero Downtime Demos
+
+01 - Create database
+
+This script creates a new database and then adds a login and user for the demos.
+A role is created as well for the demos.
+
+Copyright 2022 Steve Jones
+*/
+-- Create new database
+USE master
+GO
+ALTER DATABASE ZeroDowntime SET SINGLE_USER WITH ROLLBACK IMMEDIATE; 
+GO
+DROP DATABASE ZeroDowntime 
