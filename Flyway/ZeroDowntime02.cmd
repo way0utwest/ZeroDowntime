385111ccdc46f3cf8d1fabbfb52121f89653c918
way0utwest <sjones@sqlservercentral.com>
2023-05-08 16:28:28 -0600

    flyway renames for demo

--- Flyway/ZeroDowntime02.cmd
+++ Flyway/ZeroDowntime02.cmd
@@ -1,1 +1,3 @@
+@echo off
 flyway migrate -configFiles="./flyway.conf" -workingDirectory="E:\Documents\git\zerodowntime\Flyway" -url="jdbc:sqlserver://localhost;databaseName=ZeroDowntime;encrypt=true;integratedSecurity=true;trustServerCertificate=true" -enterprise -baselineOnMigrate=true -target=2
+echo "schema created"
